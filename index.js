43bd02bfb4a75e3ee3ce937f5aa145893221e6de
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2022-05-15 19:26:31 +0900

    now it shows the progress of download

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const fs = require("fs")
 
 const files = [
 	{
-		directory: "To Aru Majutsu No Index/To Aru Majutsu No Index II",
-		fileName: "To Aru Majutsu No Index III",
-		url_array: ["https://dondon.media/wp-content/uploads/2022/04/episodes-akame-ga-kill.jpg"],
-		format: "jpg",
+		directory: "Kaguya-Sama",
+		fileName: "Kaguya-Sama",
+		url_array: [
+			"https://descarga-directa.fukou-da.net/0:/Anime/K/Kaguya-sama%20wa%20Kokurasetai%20S2%20BD/[HanF]%20Kaguya-sama%20wa%20Kokurasetai%20S2%20-%2008%20(BDRip%201920x1080%20H264%20AAC).mp4",
+		],
+		format: "mp4",
 	},
 	{
 		directory: "To Aru Majutsu No Index/To Aru Majutsu No Index II/Ovas",
@@ -16,9 +18,8 @@ const files = [
 	},
 ]
 
-function formatFileName({ path, index, fileName, format }) {
-	//path of the file		numer of file in array			video 1     mp4
-	return `${path}/${index < 10 ? "0" + index : index} - ${fileName}.${format}`
+function formatFileName({ path, i, fileName, format }) {
+	return `${path}/${i < 10 ? "0" + i : i} - ${fileName}.${format}`
 }
 
 function createDirectory(directory) {
@@ -38,13 +39,23 @@ function createDirectory(directory) {
 }
 
 function downloadFile({ files, index, format }, callback) {
+	let lastProgress = 0
+
 	const req = https.get(files.url_array[0], (res) => {
+		const len = parseInt(res.headers["content-length"], 10)
+		let cur = ""
+		const total = len / 1048576
+
+		console.log({ len, total })
+
+		console.log("Download Started! Total Size: " + total.toFixed(2) + "mb")
+
 		if (!fs.existsSync(files.directory)) {
 			createDirectory(files.directory)
 		}
 
 		const fileStream = fs.createWriteStream(
-			formatFileName({ path: files.directory, index, fileName: files.fileName, format })
+			formatFileName({ path: files.directory, i: index, fileName: files.fileName, format })
 		)
 
 		fileStream.on("error", (err) => {
@@ -55,8 +66,24 @@ function downloadFile({ files, index, format }, callback) {
 
 		res.pipe(fileStream)
 
+		res.on("data", (chunk) => {
+			cur += chunk
+
+			const downloadedAmount = ((100.0 * cur.length) / len).toFixed(2)
+
+			if (downloadedAmount % 5 === 0 && downloadedAmount !== lastProgress) {
+				console.log(downloadedAmount + "%")
+
+				lastProgress = downloadedAmount
+			}
+		})
+
+		res.on("end", () => console.log("Download Complete!"))
+
 		fileStream.on("finish", () => {
 			fileStream.close()
+
+			console.log("Finished writting document!")
 		})
 
 		fileStream.on("close", () => callback(index))
@@ -71,9 +98,9 @@ function downloadFile({ files, index, format }, callback) {
 
 downloadFile(
 	{
-		files: files[1],
+		files: files[0],
 		index: 1,
-		format: files[1].format,
+		format: files[0].format,
 	},
 	(i) => console.log(i)
 )
