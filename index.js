bf084503afe722601a6696c6adb7ffc291e27cff
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2022-05-15 18:18:33 +0900

    created basic testing array

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const https = require("https")
 const fs = require("fs")
 const path = require("path")
 
+const https = require("https")
+const fs = require("fs")
+const path = require("path")
+
+const files = [
+	{
+		directory: "To Aru Majutsu No Index/To Aru Majutsu No Index II",
+		fileName: "To Aru Majutsu No Index III",
+		url_array: ["https://dondon.media/wp-content/uploads/2022/04/episodes-akame-ga-kill.jpg"],
+	},
+	{
+		directory: "To Aru Majutsu No Index/To Aru Majutsu No Index II/Ovas",
+		fileName: "To Aru Majutsu No Index III",
+		url_array: ["https://dondon.media/wp-content/uploads/2022/04/episodes-akame-ga-kill.jpg"],
+	},
+]
+
 function downloadFiles() {
 	console.log("Its Alive!!")
 }
