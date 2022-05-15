072aee1ce050c4d45ac4b4585cfb03f808e7c086
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2022-05-15 18:29:16 +0900

    typo

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const https = require("https")
 const fs = require("fs")
-const path = require("path")
-
-const https = require("https")
-const fs = require("fs")
-const path = require("path")
 
 const files = [
 	{
